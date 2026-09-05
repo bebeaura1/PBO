98c3bc831b6f6a8077204cabe89c7565a821bf8e
Aura Bintang Aprilian <auraaprilian0@gmail.com>
2026-09-05 23:46:34 +0700

    JS2_Langkah5Null_05

--- Jobsheet2/src/id/ac/polinema/Main.java
+++ Jobsheet2/src/id/ac/polinema/Main.java
@@ -2,9 +2,16 @@ package id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle(6, 4);
+        Rectangle original = new Rectangle(6, 4);
 
-        System.out.println("Area: " + r.area());
-        System.out.println("Perimeter: " + r.perimeter());
+        System.out.println("Area: " + original.area());
+
+        Rectangle copy = original;
+        copy.width = 10;
+        System.out.println("Via original: " + original.area());
+        System.out.println("Via copy: " + copy.area());
+
+        Rectangle empty = null; // Empty dah otomatis null
+        System.out.println(empty.area());
     }
 }
