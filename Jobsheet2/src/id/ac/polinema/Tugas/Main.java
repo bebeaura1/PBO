cc9012c1b928a6131546f5e72c417e6b6a6e1c01
Aura Bintang Aprilian <auraaprilian0@gmail.com>
2026-09-06 00:33:23 +0700

    JS2_TugasKode_05

---
+++ Jobsheet2/src/id/ac/polinema/Tugas/Main.java
@@ -0,0 +1,9 @@
+package id.ac.polinema.Tugas;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+        System.out.println("Luas Circle\t: " + c.area());
+        System.out.println("Keliling Circle\t: " + c.circumference());
+    }
+}
