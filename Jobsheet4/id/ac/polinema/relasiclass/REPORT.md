7186b85eba1ebea923c44d2910435f6912a1a347
Aura Bintang Aprilian <auraaprilian0@gmail.com>
2026-09-13 09:42:09 +0700

    JS4_Percobaan1_Langkah9_05

--- Jobsheet4/id/ac/polinema/relasiclass/REPORT.md
+++ Jobsheet4/id/ac/polinema/relasiclass/REPORT.md
@@ -11,3 +11,5 @@
 Untuk hasil running program pada percobaan 1 pada beberapa langkah yaitu seperti dibawah ini.<br>
 - Langkah 8 :<br>
 <img src="img/perc1langkah8.png" width="200px"><br>
+- Langkah 9 :<br>
+<img src="img/perc1langkah9.png" width="200px"><br>
--- Jobsheet4/id/ac/polinema/relasiclass/percobaan1/MainPercobaan1.java
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan1/MainPercobaan1.java
@@ -5,5 +5,13 @@ public class MainPercobaan1 {
         Processor p = new Processor("Inter i5", 3);
         Laptop l = new Laptop("Thinkpad", p);
         l.info();
+
+        Processor p1 = new Processor();
+        p1.setMerk("Intel i5");
+        p1.setCache(4);
+        Laptop l1 = new Laptop();
+        l1.setMerk("Thinkpad");
+        l1.setProc(p1);
+        l1.info();
     }
 }
